e00540cf52331ed2c7ae96042a10c33a1ae64945
syhily <syhily@gmail.com>
2016-12-05 15:24:22 +0800

    Fix shell command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ORANGE_CONF="/usr/local/orange/conf/orange.conf"
+NGINX_CONF="/usr/local/orange/conf/nginx.conf"
 
 # DNS resolve for nginx
 dnsmasq
@@ -14,12 +15,12 @@ if [[ "X${ORANGE_DATABASE}" != "X" ]]; then
 fi
 
 # Nginx conf modify
-if cat ${ORANGE_CONF} | grep "www www" > /dev/null
+if cat ${NGINX_CONF} | grep "www www" > /dev/null
 then
-    sed -i "s/worker_processes  4;/user www www;\nworker_processes  4;/g" ${ORANGE_CONF}
+    sed -i "s/worker_processes  4;/user www www;\nworker_processes  4;/g" ${NGINX_CONF}
 fi
-sed -i "s/resolver 114.114.114.114;/resolver 127.0.0.1 ipv6=off;/g" ${ORANGE_CONF}
-sed -i "s/lua_package_path '..\/?.lua;\/usr\/local\/lor\/?.lua;;';/lua_package_path '\/usr\/local\/orange\/?.lua;\/usr\/local\/lor\/?.lua;;';/g" ${ORANGE_CONF}
-sed -i "s/listen       80;/listen       8888;/g" ${ORANGE_CONF}
+sed -i "s/resolver 114.114.114.114;/resolver 127.0.0.1 ipv6=off;/g" ${NGINX_CONF}
+sed -i "s/lua_package_path '..\/?.lua;\/usr\/local\/lor\/?.lua;;';/lua_package_path '\/usr\/local\/orange\/?.lua;\/usr\/local\/lor\/?.lua;;';/g" ${NGINX_CONF}
+sed -i "s/listen       80;/listen       8888;/g" ${NGINX_CONF}
 
 /usr/local/bin/orange start
