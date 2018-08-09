1781736ad9c8f1364e7b9dc4991b6ebc691f3793
nopost <nopost@qq.com>
2018-08-09 16:58:13 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,6 +15,19 @@ if [[ "X${ORANGE_DATABASE}" != "X" ]]; then
     sed -i "s/\"user\": \"root\"/\"user\": \"${ORANGE_USER}\"/g" ${ORANGE_CONF}
     sed -i "s/\"password\": \"\"/\"password\": \"${ORANGE_PWD}\"/g" ${ORANGE_CONF}
 fi
+#设为true，则需用户名、密码才能登录Dashboard,默认的用户名和密码为admin/orange_admin   
+if [[ "X${DASHBOARD_AUTH}" != "X" ]]; then
+    sed -i "s/\"auth\": false/\"auth\": ${DASHBOARD_AUTH}/g" ${ORANGE_CONF}
+fi
+if [[ "X${SESSION_SECRET}" != "X" ]]; then
+    sed -i "s/\"session_secret\": \"y0ji4pdj61aaf3f11c2e65cd2263d3e7e5\"/\"session_secret\": \"${SESSION_SECRET}\"/g" ${ORANGE_CONF}
+fi
+if [[ "X${API_USERNAME}" != "X" ]]; then
+    sed -i "s/\"username\": \"api_username\"/\"username\": \"${API_USERNAME}\"/g" ${ORANGE_CONF}
+fi
+if [[ "X${API_PASSWORD}" != "X" ]]; then
+    sed -i "s/\"password\": \"api_password\"/\"password\": \"${API_PASSWORD}\"/g" ${ORANGE_CONF}
+fi
 
 # Waiting for a mysql fully started
 netConnection() {
@@ -43,15 +56,11 @@ waitForDatabase
 grep "www www" ${NGINX_CONF} > /dev/null
 if [ $? -ne 0 ];then
     sed -i "s/worker_processes  4;/user www www;\nworker_processes  4;\ndaemon  off;/g" ${NGINX_CONF}
-
     # Auto Init database for the first time
     ORANGE_DATABASE_IP=`getent hosts ${ORANGE_HOST} | awk '{ print $1 }'`
-    if [[ -z "${ORANGE_DB_SETUP}" ]]; then
-        echo "db existed"
-    else
-        echo "init db"
-        orange store -t=mysql -d=${ORANGE_DATABASE} -hh=${ORANGE_DATABASE_IP} -pp=${ORANGE_PORT} -p=${ORANGE_PWD} -u=${ORANGE_USER} -o=init -f=/usr/local/orange/install/orange-v${ORANGE_VERSION}.sql
-    fi
+    if [[ "X${ORANGE_DB_INIT}" != "X" ]]; then
+      orange store -t=mysql -d=${ORANGE_DATABASE} -hh=${ORANGE_DATABASE_IP} -pp=${ORANGE_PORT} -p=${ORANGE_PWD} -u=${ORANGE_USER} -o=init -f=/usr/local/orange/install/orange-v${ORANGE_VERSION}.sql
+    fi 
 fi
 # for k8s
 RESOLVER=`cat /etc/resolv.conf | grep nameserver  |  grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
